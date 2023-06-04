710e10abf55395126e1bb2f990fe3fcad59fbdcb
Jordan Scales <scalesjordan@gmail.com>
2023-06-04 15:51:55 -0400

    4.4

--- Main.lean
+++ Main.lean
@@ -615,3 +615,47 @@ instance [Mul α]: HMul (PPoint α) α (PPoint α) where
 
 #check HMul.hMul ({ x := 3, y := 4 } : PPoint Nat)
 -- HMul.hMul { x := 3, y := 4 } : Nat → PPoint Nat
+
+def northernTrees : Array String :=
+  #["sloe", "birch", "elm", "oak"]
+#eval northernTrees[2]
+-- #eval northernTrees[8]
+
+structure NonEmptyList (α : Type) : Type where
+  head : α
+  tail : List α
+
+def idahoSpiders : NonEmptyList String := {
+  head := "Banded Garden Spider",
+  tail := [
+    "Long-legged Sac Spider",
+    "Wolf Spider",
+    "Hobo Spider",
+    "Cat-faced Spider"
+  ]
+}
+
+def NonEmptyList.get? : NonEmptyList α → Nat → Option α
+  | xs, 0 => some xs.head
+  | xs, n + 1 => xs.tail.get? n
+
+abbrev NonEmptyList.inBounds (xs : NonEmptyList α) (i : Nat) : Prop :=
+  i ≤ xs.tail.length
+
+theorem atLeastThreeSpiders : idahoSpiders.inBounds 2 := by simp
+theorem notSixSpiders : ¬idahoSpiders.inBounds 5 := by simp
+
+def NonEmptyList.get (xs : NonEmptyList α) (i : Nat) (ok : xs.inBounds i) : α :=
+  match i with
+  | 0 => xs.head
+  | n + 1 => xs.tail[n]
+
+instance : GetElem (NonEmptyList α) Nat α NonEmptyList.inBounds where
+  getElem := NonEmptyList.get
+
+instance : GetElem (List α) Pos α (fun list n => list.length > n.toNat) where
+  getElem (xs : List α) (i : Pos) ok := xs[i.toNat]
+
+instance : GetElem (PPoint α) Bool α (fun _ _ => true) where
+  getElem (p : PPoint α) (i : Bool) _ :=
+    if ¬i then p.x else p.y
