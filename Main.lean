0a316a408544de56714b23591a90295e98b2782b
Jordan Scales <scalesjordan@gmail.com>
2023-06-18 10:25:09 -0400

    6.1

--- Main.lean
+++ Main.lean
@@ -1630,3 +1630,72 @@ def Weekday.isWeekend₃ (day : Weekday) : Bool :=
 def Weekday.isWeekend₄ : Weekday → Bool
   | .saturday | .sunday => true
   | _ => false
+
+-- Chapter 6: Functors, Applicative Functors, and Monads
+
+structure MythicalCreature where
+  large : Bool
+deriving Repr
+
+#check MythicalCreature.large
+
+structure Monster extends MythicalCreature where
+  vulnerability : String
+deriving Repr
+
+def troll : Monster where
+  large := true
+  vulnerability := "sunlight"
+
+#eval troll.toMythicalCreature
+def troll₂ : Monster := { large := true, vulnerability := "sunlight" }
+
+-- for ⟨⟩ notation, a second pair is required
+def troll₃ : Monster := ⟨⟨true⟩, "sunlight"⟩
+
+def MythicalCreature.small (c : MythicalCreature) : Bool := !c.large
+
+#eval troll₃.small
+-- Only `.` can do inheritance
+-- #eval MythicalCreature.small troll₃
+
+structure Helper extends MythicalCreature where
+  assistance : String
+  payment : String
+deriving Repr
+
+def nisse : Helper where
+  large := false
+  assistance := "househouse tasks"
+  payment := "bowl of porridge"
+
+structure MonstrousAssistant extends Monster, Helper where
+deriving Repr
+
+def domesticatedTroll : MonstrousAssistant where
+  large := true
+  vulnerability := "sunlight"
+  assistance := "household tasks"
+  payment := "bowl of porridge"
+
+inductive Size where
+  | small
+  | medium
+  | large
+deriving BEq
+
+structure SizedCreature extends MythicalCreature where
+  size : Size
+  large := size == Size.large
+
+def nonsenseCreature : SizedCreature where
+  large := false
+  size := .large
+
+abbrev SizesMatch (sc : SizedCreature) : Prop :=
+  sc.large = (sc.size == Size.large)
+
+def hulde : SizedCreature where
+  size := .medium
+
+example : SizesMatch hulde := by simp
