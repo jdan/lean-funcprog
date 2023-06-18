6a5529bcc9e9db8e8408cca5ea3b4c6eb927989d
Jordan Scales <scalesjordan@gmail.com>
2023-06-18 10:51:54 -0400

    6.2

--- Main.lean
+++ Main.lean
@@ -1699,3 +1699,117 @@ def hulde : SizedCreature where
   size := .medium
 
 example : SizesMatch hulde := by simp
+
+instance : Applicative Option where
+  pure x := .some x
+  seq f x :=
+    match f with
+    | none => none
+    -- In this case, if there is no function for seq to apply, then there is no
+    -- need to compute its argument, so x is never called
+    | some g => g <$> x ()
+
+instance : Applicative (Except ε) where
+  pure x := .ok x
+  seq f x :=
+    match f with
+    | .error e => .error e
+    | .ok g => g <$> x ()
+
+structure Pair (α β : Type) : Type where
+  first : α
+  second : β
+
+instance : Functor (Pair α) where
+  map f x := ⟨x.first, f x.second⟩
+
+-- Unfortunately, there is no Pair.pure
+
+structure RawInput where
+  name : String
+  birthYear : String
+
+def FastPos : Type := {x : Nat // x > 0}
+def one : FastPos := ⟨1, by simp⟩
+
+instance : OfNat FastPos (n + 1) where
+  ofNat := ⟨n + 1, by simp_arith⟩
+
+def Nat.asFastPos? (n : Nat) : Option FastPos :=
+  if h : n > 0 then
+    some ⟨n, h⟩
+  else
+    none
+
+structure CheckedInput (thisYear : Nat) : Type where
+  name : {n : String // n ≠ ""}
+  birthYear : {y : Nat // y > 1900 ∧ y ≤ thisYear}
+deriving Repr
+
+inductive Validate (ε α : Type) : Type where
+  | ok : α → Validate ε α
+  | errors : NonEmptyList ε → Validate ε α
+deriving Repr
+
+instance : Functor (Validate ε) where
+  map f
+    | .ok x => .ok (f x)
+    | .errors e => .errors e
+
+instance : Applicative (Validate ε) where
+  pure x := .ok x
+  seq f x :=
+    match f with
+    | .ok g => g <$> (x ())
+    | .errors errs =>
+      match x () with
+      | .ok _ => .errors errs
+      | .errors errs' => .errors (errs ++ errs')
+
+def Field := String
+def reportError (f : Field) (msg : String) : Validate (Field × String) α :=
+  .errors { head := (f, msg), tail := [] }
+
+def checkName (name : String) : Validate (Field × String) {n : String // n ≠ ""} :=
+  if h : name = "" then
+    reportError "name" "Required"
+  else
+    .ok ⟨name, h⟩
+
+def Validate.andThen (val : Validate ε α) (next : α → Validate ε β) : Validate ε β :=
+  match val with
+  | .ok x => next x
+  | .errors errs => .errors errs
+
+def checkYearIsNat (year : String) : Validate (Field × String) Nat :=
+  match year.trim.toNat? with
+  | none => reportError "birthYear" "Must be digits"
+  | some n => pure n
+
+def checkBirthYear (thisYear year : Nat) : Validate (Field × String) {y : Nat // y > 1900 ∧ y ≤ thisYear} :=
+  if h : year > 1900 then
+    if h' : year ≤ thisYear then
+      pure ⟨year, by simp [h, h']⟩
+    else reportError "birth year" s!"Must be no later than {thisYear}"
+  else reportError "birth year" "Must be after 1900"
+
+def checkInput (year : Nat) (input : RawInput) : Validate (Field × String) (CheckedInput year) :=
+  pure CheckedInput.mk <*>
+    checkName input.name <*>
+    (checkYearIsNat input.birthYear).andThen fun birthYearAsNat =>
+      checkBirthYear year birthYearAsNat
+
+#eval checkInput 2020 {name := "John", birthYear := "1980"}
+#eval checkInput 2020 {name := "", birthYear := "1980"}
+#eval checkInput 2020 {name := "John", birthYear := "1980a"}
+#eval checkInput 2020 {name := "John", birthYear := "1800"}
+#eval checkInput 2020 {name := "John", birthYear := "2100"}
+
+-- Form validation with checkInput illustrates a key advantage of Applicative
+-- over Monad. Because >>= provides enough power to modify the rest of the
+-- program's execution based on the value from the first step, it must receive a
+-- value from the first step to pass on. If no value is received (e.g. because
+-- an error has occurred), then >>= cannot execute the rest of the program.
+-- Validate demonstrates why it can be useful to run the rest of the program
+-- anyway: in cases where the earlier data isn't needed, running the rest of the
+-- program can yield useful information (in this case, more validation errors).
