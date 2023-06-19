92e28605c0ddff2f59fe80e09d7ae4200e15b62f
Jordan Scales <scalesjordan@gmail.com>
2023-06-19 09:12:10 -0400

    6.4

--- Main.lean
+++ Main.lean
@@ -1813,3 +1813,104 @@ def checkInput (year : Nat) (input : RawInput) : Validate (Field × String) (Che
 -- Validate demonstrates why it can be useful to run the rest of the program
 -- anyway: in cases where the earlier data isn't needed, running the rest of the
 -- program can yield useful information (in this case, more validation errors).
+
+abbrev NonEmptyString := {s : String // s ≠ ""}
+
+inductive LegacyCheckedInput where
+  | humanBefore1970 :
+    (birthYear : {y : Nat // y > 999 ∧ y < 1970}) →
+    String →
+    LegacyCheckedInput
+  | humanAfter1970 :
+    (birthYear : {y : Nat // y > 1970}) →
+    NonEmptyString →
+    LegacyCheckedInput
+  | company :
+    NonEmptyString →
+    LegacyCheckedInput
+deriving Repr
+
+def Validate.orElse (a : Validate ε α) (b : Unit → Validate ε α) : Validate ε α :=
+  match a with
+  | .ok x => .ok x
+  | .errors errs1 =>
+    match b () with
+    | .ok x => .ok x
+    | .errors errs2 => .errors (errs1 ++ errs2)
+
+-- E1 <|> E2 will use the built-in OrElse.orElse
+instance : OrElse (Validate ε α) where
+  orElse := Validate.orElse
+
+def checkThat (condition : Bool) (field : Field) (msg : String) : Validate (Field × String) Unit :=
+  if condition then pure () else reportError field msg
+
+def checkCompany (input : RawInput) : Validate (Field × String) LegacyCheckedInput :=
+  pure (fun () name => .company name) <*>
+    checkThat (input.birthYear == "FIRM") "birth year" "FIRM if a company" <*>
+    checkName input.name
+-- this definition is quite noisy
+
+-- class SeqRight (f : Type → Type) where
+--   seqRight : f α → (Unit → f β) → f β
+
+def checkCompany₂ (input : RawInput) : Validate (Field × String) LegacyCheckedInput :=
+  checkThat (input.birthYear == "FIRM") "birth year" "FIRM if a company" *>
+    pure .company <*> checkName input.name
+
+def checkCompany₃ (input : RawInput) : Validate (Field × String) LegacyCheckedInput :=
+  checkThat (input.birthYear == "FIRM") "birth year" "FIRM if a company" *>
+  .company <$> checkName input.name
+
+def checkSubtype {α : Type} (v : α) (p : α → Prop) [Decidable (p v)] (err : ε) : Validate ε {x : α // p x} :=
+  if h : p v then
+    pure ⟨v, h⟩
+  else
+    .errors { head := err, tail := []}
+
+def checkHumanBefore1970 (input : RawInput) : Validate (Field × String) LegacyCheckedInput :=
+  (checkYearIsNat input.birthYear).andThen fun y =>
+    .humanBefore1970 <$>
+      checkSubtype y (fun x => x > 999 ∧ x < 1970) ("birth year", "less than 1970") <*>
+      pure input.name
+
+def checkHumanAfter1970 (input : RawInput) : Validate (Field × String) LegacyCheckedInput :=
+  (checkYearIsNat input.birthYear).andThen fun y =>
+    .humanAfter1970 <$>
+      checkSubtype y (· > 1970) ("birth year", "greater than 1970") <*>
+      checkName input.name
+
+def checkLegacyInput (input : RawInput) : Validate (Field × String) LegacyCheckedInput :=
+  checkCompany input <|> checkHumanBefore1970 input <|> checkHumanAfter1970 input
+
+#eval checkLegacyInput ⟨"Johnny's Troll Groomers", "FIRM"⟩
+#eval checkLegacyInput ⟨"Johnny", "1980"⟩
+#eval checkLegacyInput ⟨"", "1963"⟩
+#eval checkLegacyInput ⟨"", "1970"⟩
+
+-- Alternative can help here
+-- class Alternative (f : Type → Type) extends Applicative f where
+--   failure : f α
+--   orElse : f α → (Unit → f α) → f α
+-- instance [Alternative f] : OrElse (f α) where
+--   orElse := Alternative.orElse
+
+def Many.orElse : Many α → (Unit → Many α) → Many α
+  | .none, ys => ys ()
+  | .more x xs, ys => .more x (fun () => orElse (xs ()) ys)
+instance : Alternative Many where
+  failure := .none
+  orElse := Many.orElse
+
+def Many.countdown : Nat → Many Nat
+  | 0 => .none
+  | n + 1 => .more n (fun () => countdown n)
+def evenDivisors (n : Nat) : Many Nat := do
+  let k ← Many.countdown (n + 1)
+  guard (k % 2 = 0)
+  guard (n % k = 0)
+  pure k
+
+#eval (evenDivisors 20).takeAll
+
+-- Skipping the exercises, this chapter is pretty gnarly
